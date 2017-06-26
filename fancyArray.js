d4209368545135aa2b6b10bb546c6cf1910c36a2
Bald Mike <baldmike312@gmail.com>
2017-06-26 13:08:45 -0500

    quick add

---
+++ fancyArray.js
@@ -0,0 +1,9 @@
+function fancyArray(arr) {
+    for (var i=0; i < arr.length; i++) {
+        console.log(i + "-->" + arr[i]);
+    }
+}
+
+
+var myArr = ["dog", 4, 6, "cat", 2, 3]
+fancyArray(myArr);
